9f0c528d3557e65c86f7dd24ff1ca5759d00573f
Adam Watters <adamwatters11@gmail.com>
2019-02-21 06:13:37 -0500

    make request and render people array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
 
+const URL = "https://swapi.co/api/people/";
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: []
+    };
+  }
+  componentDidMount() {
+    fetch(URL)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ people: json.results });
+      });
+  }
   render() {
-    return <div className="App" />;
+    const { people } = this.state;
+    return (
+      <div className="App">
+        {people.map(person => {
+          return person.name;
+        })}
+      </div>
+    );
   }
 }
 
