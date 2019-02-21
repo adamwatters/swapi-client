e23fe4b9b1caea199302565d44cb837ee36a3aab
Adam Watters <adamwatters11@gmail.com>
2019-02-21 07:18:19 -0500

    basic styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 const URL = "https://swapi.co/api/people/";
+const ICON_CLASSES = {
+  droid: "fa-android",
+  human: "fa-user-circle",
+  warning: "fa-exclamation-triangle"
+};
 
 class App extends Component {
   constructor() {
@@ -20,9 +25,37 @@ class App extends Component {
     const { people } = this.state;
     return (
       <div className="App">
-        {people.map(person => {
-          return person.name;
-        })}
+        <div className="controls">
+          <div className="search">
+            <input placeholder="Search..." type="text" />
+          </div>
+          <div className="sort">
+            <input placeholder="Sort results..." type="select" />
+          </div>
+        </div>
+        <div className="list">
+          {people.map(person => {
+            // basic style - need to implement API to get species
+            console.log(person.species);
+            const iconClass = `fas ${ICON_CLASSES[person.species] ||
+              ICON_CLASSES["warning"]}`;
+            return (
+              <div className="item">
+                <div className="name">
+                  <div className={`icon ${iconClass}`} />
+                  {person.name}
+                </div>
+                <div className="stats">
+                  {`height: ${person.height}`}
+                  <div className="stats-divider" />
+                  {`mass: ${person.mass}`}
+                  <div className="stats-divider" />
+                  {`gender: ${person.gender}`}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
